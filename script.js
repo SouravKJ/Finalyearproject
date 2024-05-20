b52bbd90dceb8da4e171dc57e2555849bb759ace
sourav jha <souravjha1324@gmail.com>
2024-05-20 21:28:20 +0530

    add existing file1

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    var name = prompt("Please enter your name:");
+   // var name = prompt("Please enter your name:");
     if (name) {
         document.getElementById("username").textContent = name;
     } else {
